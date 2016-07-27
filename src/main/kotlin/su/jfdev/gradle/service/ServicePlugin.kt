95f234a2503a357a7d578e86415dbd6648c1d544
Jamefrus <jamefrus@ya.ru>
2016-07-27 20:48:05 +0300

    FIXED: sourceSets instead project

--- src/main/kotlin/su/jfdev/gradle/service/ServicePlugin.kt
+++ src/main/kotlin/su/jfdev/gradle/service/ServicePlugin.kt
@@ -8,11 +8,12 @@ import su.jfdev.gradle.service.util.*
 
 class ServicePlugin: Plugin<Project> {
     override fun apply(project: Project) {
+        val sourceSets = project.sourceSets
         with(project) {
             addConfigurationDummies("api", "spec", "impl")
             extensions.create("require", ParallelRequireUser::class.java, project)
-            extensions.create("services", ImplementDescriber::class.java, project)
-            extensions.create("describe", AdditionalDescriber::class.java, project)
+            extensions.create("services", ImplementDescriber::class.java, sourceSets)
+            extensions.create("describe", AdditionalDescriber::class.java, sourceSets)
         }
     }
 
