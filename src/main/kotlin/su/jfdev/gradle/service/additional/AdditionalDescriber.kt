bc92483df97e8dd220cce05babb43169b43a6444
Jamefrus <jamefrus@ya.ru>
2016-07-28 23:08:40 +0300

    Refactoring

--- src/main/kotlin/su/jfdev/gradle/service/additional/AdditionalDescriber.kt
+++ src/main/kotlin/su/jfdev/gradle/service/additional/AdditionalDescriber.kt
@@ -18,39 +18,41 @@ open class AdditionalDescriber(val ext: ServiceExtension) {
             = additionalSources[additional]!!
 
     operator fun set(additional: AdditionalSources, alias: String) = get(additional).add(alias).apply {
-        ext.project.require(ext.project.path) {
-            for (upper in additional.upper)
-                for (fromSet in replace(upper))
-                    sources(fromSet, alias)
+        configure(additional, alias)
+    }
 
-            for (downer in additional.downer)
-                for (toSet in replace(downer))
-                    sources(alias, toSet)
+    private fun configure(additional: AdditionalSources, alias: String) {
+        ext.project.require(ext.project.path) {
+            for ((key, value) in additional.upper)
+                for (fromSet in replace(value))
+                    sources(fromSet, alias, key)
+
+            for ((key, value) in additional.downer)
+                for (toSet in replace(value))
+                    sources(alias, toSet, key)
         }
     }
 
     private fun replace(name: String): Iterable<String> {
         val sources = AdditionalSources[name] ?: return listOf(name)
-        val container = get(sources)
-        return when {
-            container.isEmpty -> emptyList()
-            else              -> container.map { it.name }
+        return get(sources).run {
+            when {
+                isEmpty -> emptyList()
+                else    -> map { it.name }
+            }
         }
     }
 
     @JvmOverloads fun api(alias: String = "api") = set(api, alias)
-
     @JvmOverloads fun spec(alias: String = "spec") = set(spec, alias)
+    @JvmOverloads fun impl(alias: String = "impl") = set(impl, alias)
 
     fun impl(map: Map<String, Map<String, Iterable<String>>>) {
         ext.implementations.add(map)
         for ((key, value) in map) impl(key)
     }
-    fun impl() = impl("impl")
 
-    fun impl(vararg aliases: String) = aliases.forEach {
-        set(impl, it)
-    }
+    fun impl(vararg aliases: String) = aliases.forEach { impl(it) }
 
     companion object {
         operator fun get(project: Project): AdditionalDescriber = project.extension.describe
