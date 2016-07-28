a3de6c53f8cd66f050ba8e1fc4ce64b51c69d15a
Jamefrus <jamefrus@ya.ru>
2016-07-28 17:26:41 +0300

    Using userProject instead rootProject

--- src/test/kotlin/su/jfdev/gradle/service/ServiceUserSpec.groovy
+++ src/test/kotlin/su/jfdev/gradle/service/ServiceUserSpec.groovy
@@ -1,5 +1,6 @@
 package su.jfdev.gradle.service
 
+import org.gradle.api.Project
 import spock.lang.Ignore
 import su.jfdev.gradle.service.util.DependencyWithSources
 
@@ -7,10 +8,17 @@ import static su.jfdev.gradle.service.util.ConfigNameKt.makeConfigName
 import static su.jfdev.gradle.service.util.ConfigNameKt.scopes
 
 class ServiceUserSpec extends ServicePluginSpec {
+    Project userProject
+    Project ownerProject
+
+    @Override
+    void setup() {
+        userProject = addSubproject("user")
+        ownerProject = addSubproject("owner")
+    }
+
     def "should add dependency with def impl"() {
         given:
-        def userProject = addSubproject("user")
-        def ownerProject = addSubproject("owner")
         project.subprojects {
             apply plugin: "java"
             apply plugin: ServicePlugin
@@ -51,7 +59,7 @@ class ServiceUserSpec extends ServicePluginSpec {
     void contains(boolean expect = true, String... keys) {
         for (key in keys) for (scope in scopes) for (sources in ["api", "main", "impl"]) {
             def name = makeConfigName(sources, scope)
-            def conf = project.configurations.findByName(name)
+            def conf = userProject.configurations.findByName(name)
             def result = expect == conf.dependencies.any {
                 it instanceof DependencyWithSources &&
                         it.project.path == ":$key" &&
