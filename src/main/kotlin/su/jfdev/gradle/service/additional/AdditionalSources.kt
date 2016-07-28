6e6ad1a856238da953aff1de8c8e47c96dd08719
Jamefrus <jamefrus@ya.ru>
2016-07-28 22:03:50 +0300

    Using Map<> instead Iterable<>

--- src/main/kotlin/su/jfdev/gradle/service/additional/AdditionalSources.kt
+++ src/main/kotlin/su/jfdev/gradle/service/additional/AdditionalSources.kt
@@ -4,21 +4,21 @@ import org.gradle.api.tasks.*
 
 enum class AdditionalSources(val repeatable: Boolean = false,
                              val alternative: String? = null,
-                             val upper: Iterable<String> = emptyList(),
-                             val downer: Iterable<String> = emptyList()) {
+                             val upper: Map<String, String> = emptyMap(),
+                             val downer: Map<String, String> = emptyMap()) {
     api(
                alternative = "main",
-               downer = listOf("main")
+               downer = mapOf("compile" to "main")
        ),
     spec(
                 alternative = "test",
-                downer = listOf("test"),
-                upper = listOf("impl")
+                downer =  mapOf("compile" to "test"),
+                upper =  mapOf("runtime" to "impl")
         ),
     impl(
                 alternative = "impl",
                 repeatable = true,
-                upper = listOf("main")
+                upper = mapOf("runtime" to "main")
         );
 
     operator fun get(sourceSets: SourceSetContainer) = AdditionalContainer.smart(sourceSets, !repeatable)
