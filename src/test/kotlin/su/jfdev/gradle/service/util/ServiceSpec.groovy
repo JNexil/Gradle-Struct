53c960399f98b5d0278a8bce5e669de8cfbd44a5
Jamefrus <jamefrus@ya.ru>
2016-08-01 14:17:51 +0300

    Add shared annotation

--- src/test/kotlin/su/jfdev/gradle/service/util/ServiceSpec.groovy
+++ src/test/kotlin/su/jfdev/gradle/service/util/ServiceSpec.groovy
@@ -2,18 +2,20 @@ package su.jfdev.gradle.service.util
 
 import nebula.test.ProjectSpec
 import su.jfdev.gradle.service.plugin.ServicePlugin
+import spock.lang.Shared as S
 
-import static su.jfdev.gradle.service.util.ContainsChecker.*
+import static su.jfdev.gradle.service.util.ContainsChecker.checker
 
 class ServiceSpec extends ProjectSpec {
-    Collection<String> knownSources
-    Collection<String> knownConfigurations
+    @S Collection<String> knownSources
+    @S Collection<String> knownConfigurations
 
-    Collection<String> unknownSources
-    Collection<String> unknownConfigurations
+    @S Collection<String> unknownSources
+    @S Collection<String> unknownConfigurations
 
     void setup() {
         project.plugins.apply(ServicePlugin)
+
         knownSources = checker(project.sourceSets)
         knownConfigurations = checker(project.configurations)
 
