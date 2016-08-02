a489cf3ac7ecc844b2cecbc997658cd193ddc6d1
Jamefrus <jamefrus@ya.ru>
2016-08-02 15:10:12 +0300

    Default service()

--- src/main/kotlin/su/jfdev/gradle/service/require/Require.kt
+++ src/main/kotlin/su/jfdev/gradle/service/require/Require.kt
@@ -6,16 +6,13 @@ import su.jfdev.gradle.service.describe.Scope.*
 import su.jfdev.gradle.service.util.*
 
 class Require(val receiver: Module, val target: Module): Closure<Any>(Unit) {
-    fun service() {
-        val default = target.service.impl.firstOrNull() ?: error("Missing implementations for ${target.path}")
-        service(default.name)
-    }
 
     fun service(vararg implementations: String) {
         source("api")
         source("main")
         source("spec")
-        for (implementation in implementations)
+        val orDefault = if(implementations.isEmpty()) defaultImplementation else implementations.toList()
+        for (implementation in orDefault)
             source(implementation, to = "test", scope = RUNTIME)
     }
 
@@ -41,5 +38,10 @@ class Require(val receiver: Module, val target: Module): Closure<Any>(Unit) {
         val closure = function.clone() as Closure<*>
         closure.delegate = this
         closure.call()
+    }
+
+    private val defaultImplementation: Collection<String> get() {
+        val def = target.service.impl.firstOrNull() ?: return emptyList()
+        return listOf(def).map { it.name }
     }
 }
