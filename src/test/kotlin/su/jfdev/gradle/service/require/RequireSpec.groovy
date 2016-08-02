84f85588b7ef2abdd7966a843f43515d1fec4664
Jamefrus <jamefrus@ya.ru>
2016-08-02 21:56:57 +0300

    True require checking

--- src/test/kotlin/su/jfdev/gradle/service/require/RequireSpec.groovy
+++ src/test/kotlin/su/jfdev/gradle/service/require/RequireSpec.groovy
@@ -1,40 +1,105 @@
 package su.jfdev.gradle.service.require
 
 import org.gradle.api.Project
+import org.gradle.api.artifacts.Configuration
+import spock.lang.Unroll
 import su.jfdev.gradle.service.describe.ConfigurationDependency
+import su.jfdev.gradle.service.describe.Scope
 import su.jfdev.gradle.service.spec.ServiceSpock
 
+import static su.jfdev.gradle.service.describe.Scope.COMPILE
+import static su.jfdev.gradle.service.describe.Scope.RUNTIME
+
 class RequireSpec extends ServiceSpock {
-    Project first
-    Project second
+    public static final ALL = ["api", "main", "impl", "spec", "test"]
+    Project target
+    Project receiver
 
     @Override
     void setup() {
-        first = addSubproject("first")
-        second = addSubproject("second")
-        serviceTo(first, second)
-        first.service {
+        serviceTo(
+                target = addSubproject("target"),
+                receiver = addSubproject("receiver")
+        )
+        target.service {
+            api()
+            spec()
+            impl()
+        }
+        receiver.service {
             api()
             spec()
             impl()
         }
     }
 
-    def "should add single sourceSet"() {
+    @Unroll
+    def "should add `#source` with #transitive"() {
         given:
-        second.require.from(":first") {
-            compile "api"
-        }
+        receiver.require.from(":target").compile source
+        def exclusions = ALL - transitive - source
 
         expect:
-        wasRequired "apiCompile"
+        wasRequired(COMPILE, source)
+
+        and:
+        assertNonRequired(COMPILE, source, exclusions)
+
+        and:
+        assertRequired(COMPILE, source, transitive)
+
+        where:
+        source | transitive
+        "api"  | []
+        "main" | ["api"]
+        "impl" | ["api", "main"]
+        "spec" | ["api", "main"]
+        "test" | ["api", "main", "spec", "impl"]
     }
 
-    private boolean wasRequired(String to, String from = to) {
-        second.configurations.getByName(to).dependencies.any {
-            it instanceof ConfigurationDependency &&
-                    it.configuration.name == from &&
-                    it.target.path == first.path
+    @Unroll
+    def "should when add service, add `#source` with #requiredFrom without #nonRequiredFrom"() {
+        given:
+        receiver.require.service ":target"
+
+        expect:
+        wasRequired(scope, source)
+
+        where:
+        source | scope
+        "api"  | COMPILE
+        "main" | COMPILE
+        "impl" | RUNTIME
+        "spec" | COMPILE
+    }
+
+    private void assertNonRequired(Scope scope, String receiver, List<String> target) {
+        for (String fromEntry : target)
+            assert wasNotRequired(scope, receiver, fromEntry)
+    }
+
+    private void assertRequired(Scope scope, String receiver, List<String> target) {
+        for (String fromEntry : target)
+            assert wasRequired(scope, receiver, fromEntry)
+    }
+
+    private boolean wasNotRequired(Scope scope, String receiver, String target = receiver) {
+        !wasRequired(scope, receiver, target)
+    }
+
+    private boolean wasRequired(Scope scope, String receiver, String target = receiver) {
+        String $receiver = scope[receiver]
+        String $target = scope[target]
+        def configuration = this.receiver.configurations.getByName($receiver)
+        isRequired(configuration, $target)
+    }
+
+    private boolean isRequired(Configuration configuration, String $target) {
+        configuration.dependencies.any {
+            if (it instanceof ConfigurationDependency && it.target.path == this.target.path) {
+                def isTarget = it.configuration.name == $target
+                isTarget || isRequired(it.configuration, $target)
+            } else false
         }
     }
 }
