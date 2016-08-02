2ec821733b39a1b4bdcbb6186f1cbc2fbf3d3aa5
Jamefrus <jamefrus@ya.ru>
2016-08-02 14:58:03 +0300

    short access to service(impls)

--- src/main/kotlin/su/jfdev/gradle/service/require/RequireExtension.kt
+++ src/main/kotlin/su/jfdev/gradle/service/require/RequireExtension.kt
@@ -6,6 +6,7 @@ import su.jfdev.gradle.service.util.*
 open class RequireExtension(val project: Project) {
     val to: RequireExtension = this
     fun to(to: String) = get(to)
+    fun service(to: String, vararg implementations: String) = get(to).service(*implementations)
 
     operator fun get(to: String) = Require(project.module, project.project(to).module)
 }
