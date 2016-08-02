2865186f3f890c52a88a3bcc021543d6bfe972af
Jamefrus <jamefrus@ya.ru>
2016-08-02 15:50:57 +0300

    from() with closure

--- src/main/kotlin/su/jfdev/gradle/service/require/RequireExtension.kt
+++ src/main/kotlin/su/jfdev/gradle/service/require/RequireExtension.kt
@@ -1,12 +1,15 @@
 package su.jfdev.gradle.service.require
 
+import groovy.lang.*
 import org.gradle.api.*
 import su.jfdev.gradle.service.util.*
 
 open class RequireExtension(val project: Project) {
     val from: RequireExtension = this
+    fun from(target: String, closure: Closure<*>) = from(target).doCall(closure)
     fun from(target: String) = get(target)
     fun service(to: String, vararg implementations: String) = get(to).service(*implementations)
 
+    @JvmName("getAt")
     operator fun get(from: String) = Require(project.module, project.project(from).module)
 }
