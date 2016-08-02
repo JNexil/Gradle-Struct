14ebebf8fb06971f45f7bdae29b4606c17aafe2e
Jamefrus <jamefrus@ya.ru>
2016-08-02 16:07:28 +0300

    Applied plugin to subprojects

--- src/test/kotlin/su/jfdev/gradle/service/require/RequireSpec.groovy
+++ src/test/kotlin/su/jfdev/gradle/service/require/RequireSpec.groovy
@@ -1,7 +1,40 @@
 package su.jfdev.gradle.service.require
 
+import org.gradle.api.Project
+import su.jfdev.gradle.service.describe.ConfigurationDependency
 import su.jfdev.gradle.service.spec.ServiceSpock
 
 class RequireSpec extends ServiceSpock {
+    Project first
+    Project second
 
+    @Override
+    void setup() {
+        first = addSubproject("first")
+        second = addSubproject("second")
+        serviceTo(first, second)
+        first.service {
+            api()
+            spec()
+            impl()
+        }
+    }
+
+    def "should add single sourceSet"() {
+        given:
+        second.require.from(":first") {
+            source "api"
+        }
+
+        expect:
+        wasRequired("apiCompile", "apiCompile")
+    }
+
+    private boolean wasRequired(String to, String from) {
+        second.configurations.getByName(to).dependencies.any {
+            it instanceof ConfigurationDependency &&
+                    it.configuration.name == from &&
+                    it.target.path == first.path
+        }
+    }
 }
--- src/test/kotlin/su/jfdev/gradle/service/spec/ServiceSpock.groovy
+++ src/test/kotlin/su/jfdev/gradle/service/spec/ServiceSpock.groovy
@@ -1,9 +1,14 @@
 package su.jfdev.gradle.service.spec
 
+import org.gradle.api.Project
 import su.jfdev.gradle.service.plugin.ServicePlugin
 
 abstract class ServiceSpock extends nebula.test.ProjectSpec {
     void setup() {
-        project.plugins.apply(ServicePlugin)
+        serviceTo(project)
+    }
+
+    public void serviceTo(Project... projects) {
+        for(project in projects) project.plugins.apply(ServicePlugin)
     }
 }
