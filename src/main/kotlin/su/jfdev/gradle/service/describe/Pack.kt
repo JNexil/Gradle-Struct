82814d0221fafa1fa234e4cf448f389f385e8e3d
Jamefrus <jamefrus@ya.ru>
2016-08-04 19:50:12 +0300

    Replaced Module with Project, constructor with dummy replaced by companion method

--- src/main/kotlin/su/jfdev/gradle/service/describe/Pack.kt
+++ src/main/kotlin/su/jfdev/gradle/service/describe/Pack.kt
@@ -1,13 +1,24 @@
 package su.jfdev.gradle.service.describe
 
+import org.gradle.api.*
 import org.gradle.api.tasks.*
 import su.jfdev.gradle.service.dependency.*
+import su.jfdev.gradle.service.util.*
 
-class Pack(val module: Module, val name: String) {
-    val sourceSet: SourceSet = module.sources.maybeCreate(name)
+data class Pack(val project: Project, val name: String) {
+
+    val sourceSet: SourceSet? get() = project.sourceSets.findByName(name)
+
     val configurations: Map<Scope, PackDependency> = Scope.values().associate {
         it to PackDependency(this, it)
     }
 
+    @JvmName("getAt")
     operator fun get(scope: Scope) = configurations[scope]!!
+
+    companion object {
+        operator fun get(project: Project, name: String) = Pack(project, name).apply {
+            project.sourceSets.maybeCreate(name)
+        }
+    }
 }
