49c7e7cd5619b902afa3024bef7f32d1b98488c4
Jamefrus <jamefrus@ya.ru>
2016-08-05 16:48:33 +0300

    Replaced manual building to from source set extracting

--- src/main/kotlin/su/jfdev/gradle/service/describe/Scope.kt
+++ src/main/kotlin/su/jfdev/gradle/service/describe/Scope.kt
@@ -1,12 +1,17 @@
 package su.jfdev.gradle.service.describe
 
-enum class Scope(val scope: String) {
-    RUNTIME("runtime"),
-    COMPILE("compile");
+import org.gradle.api.*
+import org.gradle.api.artifacts.*
+import org.gradle.api.tasks.*
+import su.jfdev.gradle.service.util.*
+
+enum class Scope(private val extractor: SourceSet.() -> String) {
+    RUNTIME({ runtimeConfigurationName }),
+    COMPILE({ compileConfigurationName });
 
     @JvmName("getAt")
-    operator fun get(name: String) = when (name) {
-        "main" -> scope
-        else   -> name.decapitalize() + scope.capitalize()
+    operator fun get(project: Project, name: String): Configuration {
+        val configuration = project.sourceSets.getByName(name).extractor()
+        return project.configurations.getByName(configuration)
     }
 }
