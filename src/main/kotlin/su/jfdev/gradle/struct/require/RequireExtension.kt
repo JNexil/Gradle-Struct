c4eb6f514b2e4ce76ec6d00a391b8bb061297f66
Jamefrus <jamefrus@ya.ru>
2016-08-06 10:37:25 +0300

    Cached Require's

--- src/main/kotlin/su/jfdev/gradle/struct/require/RequireExtension.kt
+++ src/main/kotlin/su/jfdev/gradle/struct/require/RequireExtension.kt
@@ -2,13 +2,18 @@ package su.jfdev.gradle.struct.require
 
 import groovy.lang.*
 import org.gradle.api.*
+import java.util.*
 
 open class RequireExtension(val project: Project) {
+    private val templates: MutableMap<String, Require> = HashMap()
+
     val from: RequireExtension = this
     fun from(target: String, closure: Closure<*>) = from(target).doCall(closure)
     fun from(target: String) = get(target)
     fun template(to: String, vararg implementations: String) = get(to).template(*implementations)
 
     @JvmName("getAt")
-    operator fun get(from: String) = Require(project, project.project(from))
+    operator fun get(from: String) = templates.getOrPut(from){
+        Require(project, project.project(from))
+    }
 }
