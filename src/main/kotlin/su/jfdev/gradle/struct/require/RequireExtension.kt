18163037123e8b4cc03dca3f6b51b11e8b0ba655
Jamefrus <jamefrus@ya.ru>
2016-08-06 10:57:17 +0300

    Template inheriting

--- src/main/kotlin/su/jfdev/gradle/struct/require/RequireExtension.kt
+++ src/main/kotlin/su/jfdev/gradle/struct/require/RequireExtension.kt
@@ -7,10 +7,13 @@ import java.util.*
 open class RequireExtension(val project: Project) {
     private val templates: MutableMap<String, Require> = HashMap()
 
+    val required: Collection<Require> get() = templates.values
+
     val from: RequireExtension = this
     fun from(target: String, closure: Closure<*>) = from(target).doCall(closure)
     fun from(target: String) = get(target)
-    fun template(to: String, vararg implementations: String) = get(to).template(*implementations)
+    fun template(from: String, vararg implementations: String) = get(from).template(*implementations)
+    fun inherit(from: String, vararg implementations: String) = get(from).inherit(*implementations)
 
     @JvmName("getAt")
     operator fun get(from: String) = templates.getOrPut(from){
