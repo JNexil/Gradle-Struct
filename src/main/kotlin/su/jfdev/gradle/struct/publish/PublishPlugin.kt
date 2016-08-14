b03bfbc43b7bf7d683117ad8f42db87188f7ce03
Jamefrus <jamefrus@ya.ru>
2016-08-14 18:13:04 +0300

    Lazy publications archiving

--- src/main/kotlin/su/jfdev/gradle/struct/publish/PublishPlugin.kt
+++ src/main/kotlin/su/jfdev/gradle/struct/publish/PublishPlugin.kt
@@ -6,34 +6,63 @@ import org.gradle.api.publish.maven.*
 import org.gradle.api.tasks.bundling.*
 import su.jfdev.gradle.struct.describe.*
 import su.jfdev.gradle.struct.util.*
+import java.util.*
 
 class PublishPlugin: Plugin<Project> {
     lateinit var project: Project
+    val archives: Iterable<String> = Archives()
+
     override fun apply(project: Project) {
         this.project = project
-        with(project) {
-            task("structUpload").doFirst {
-                onlyWith<PublishingExtension>("publishing") {
-                    onlyWith<NamedDomainObjectContainer<Pack>>("describe") {
-                        forEach {
-                            publications.publish(it)
-                        }
-                    }
+        project.extensions.add("structArchives", archives)
+    }
+
+    private val Pack.toArchive: String? get() {
+        project.onlyWith<PublishingExtension>("publishing") {
+            return publications publish this@toArchive
+        }
+        return null
+    }
+
+    private infix fun PublicationContainer.publish(pack: Pack): String? {
+        val archive = pack.archive ?: return null
+        return publish(archive, pack).name
+    }
+
+    private fun PublicationContainer.publish(archive: String, pack: Pack): MavenPublication
+            = maybeCreate(archive, MavenPublication::class.java)
+            .apply { configure(archive, pack) }
+
+    private fun MavenPublication.configure(archive: String, pack: Pack) {
+        artifactId = archive
+        groupId = project.group.toString()
+        version = project.version.toString()
+
+        artifact(pack toJar archive)
+    }
+
+    private infix fun Pack.toJar(archive: String): Jar = project.tasks.maybeCreate(archive + "Jar", Jar::class.java).apply {
+        from(sourceSet.output)
+    }
+
+    private inner class Archives: Iterable<String> {
+        private val archives = HashSet<String>()
+        private val extension: NamedDomainObjectContainer<Pack>? get() = project.extensions["describe"]
+        private var listened: Boolean = false
+
+        override fun iterator(): Iterator<String> {
+            if (!listened) extension?.listen()
+            return archives.iterator()
+        }
+
+        private fun NamedDomainObjectContainer<Pack>.listen() {
+            listened = true
+            whenObjectAdded { pack ->
+                val archive = pack.toArchive
+                if(archive != null){
+                    archives += archive
                 }
             }
         }
     }
-
-    fun PublicationContainer.publish(pack: Pack) {
-        val archive = pack.archive ?: return
-        val task = project.tasks.maybeCreate(archive + "Jar", Jar::class.java)
-        maybeCreate(archive, MavenPublication::class.java).apply {
-            artifactId = archive
-            groupId = project.group.toString()
-            version = project.version.toString()
-
-            artifact(task)
-        }
-    }
-
 }
--- src/main/kotlin/su/jfdev/gradle/struct/util/Project.kt
+++ src/main/kotlin/su/jfdev/gradle/struct/util/Project.kt
@@ -1,11 +1,14 @@
 package su.jfdev.gradle.struct.util
 
 import org.gradle.api.*
+import org.gradle.api.plugins.*
 import su.jfdev.gradle.struct.describe.*
 
 operator fun Project.get(name: String) = Pack(this, name)
 
-inline fun <reified T: Any> Project.onlyWith(name: String, block: T.() -> Unit) {
-    val extension = extensions.findByName(name) ?: return
-    if (extension is T) block(extension)
+inline operator fun <reified T> ExtensionContainer.get(name: String) = findByName(name) as? T
+
+inline fun <reified T: Any> Project.onlyWith(name: String, action: T.() -> Unit) {
+    val value: T = extensions[name] ?: return
+    value.action()
 }
