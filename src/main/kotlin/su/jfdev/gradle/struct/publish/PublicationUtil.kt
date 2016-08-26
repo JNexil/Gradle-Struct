5380dd4ba58f96f6aa313cba580ef989d4828a1b
Jamefrus <jamefrus@ya.ru>
2016-08-26 07:35:46 +0300

    Refactored linePath

--- src/main/kotlin/su/jfdev/gradle/struct/publish/PublicationUtil.kt
+++ src/main/kotlin/su/jfdev/gradle/struct/publish/PublicationUtil.kt
@@ -9,11 +9,13 @@ import su.jfdev.gradle.struct.util.*
 val Pack.publication: MavenPublication get() = publications
         .maybeCreate(camelPath, MavenPublication::class.java)
         .apply {
-            artifactId = path.joinToString(separator = "-")
+            artifactId = linePath
             groupId = project.group.toString()
             version = project.version.toString()
         }
 
 val Pack.publications: PublicationContainer get() = publishing.publications
 val Pack.publishing: PublishingExtension get() = project.extensions.getByType(PublishingExtension::class.java)
+
 val Pack.camelPath: String get() = path.joinToString(transform = String::capitalize, separator = "")
+val Pack.linePath: String get() = path.joinToString(separator = "-")
